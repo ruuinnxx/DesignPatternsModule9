dcbd7959a91d3eced535b1d018730985d27e6cd9
ruuinnxx <tokzhigitova.gb@gmail.com>
2025-11-11 22:39:14 +0500

    Первый коммит с заданиями

---
+++ HomeWork9/Project1_Facade/Program.cs
@@ -0,0 +1,106 @@
+﻿﻿var homeTheaterFacade = 
+    new HomeTheaterFacade(new AudioSystem(), new Tv(), new DvdPlayer(), new GameConsole());
+homeTheaterFacade.SartDvdPlayer();
+homeTheaterFacade.StartTv();
+homeTheaterFacade.EndDvd();
+homeTheaterFacade.EndTv();
+homeTheaterFacade.PlayGameStart();
+homeTheaterFacade.PlayGameStop();
+
+
+#region Классы под системы
+
+public class AudioSystem
+{
+    public void TurnOn() =>
+        Console.WriteLine("Включение Аудио системы");
+    
+    public void SetVolume(int level) =>
+        Console.WriteLine($"Установка громкости звука: {level}");
+    
+    public void TurnOff() =>
+        Console.WriteLine("Выключение аудио системы");
+}
+
+public class Tv
+{
+    public void TurnOn() =>
+        Console.WriteLine("Включение Tv");
+
+    public void SetChannel(int channel) =>
+        Console.WriteLine($"Переключение канала: {channel}");
+    
+    public void TurnOff() =>
+        Console.WriteLine("Выключение Tv");
+}
+
+public class DvdPlayer
+{
+    public void PlaybackDvd() =>
+        Console.WriteLine("Воспроизведение DVD-диска");
+
+    public void PauseDvd() =>
+        Console.WriteLine("Пауза видео плеера");
+    
+    public void StopDvd() =>
+        Console.WriteLine("Остановка DVD-диска");
+}
+
+public class GameConsole
+{
+    public void TurnOnGame() =>
+        Console.WriteLine("Включение консоли, запуск игры");
+
+    public void StartGame() =>
+        Console.WriteLine("Запуск игры");
+}
+
+#endregion
+
+#region Фасад
+
+public class HomeTheaterFacade(AudioSystem audio, Tv tv, DvdPlayer dvdPlayer, GameConsole gameConsole)
+{
+    public void StartTv()
+    {
+        Console.WriteLine("Подготовка к просмотру Tv: ");
+        tv.TurnOn();
+        tv.SetChannel(5);
+        audio.TurnOn();
+        audio.SetVolume(8);
+        Console.WriteLine("Tv готов к просмотру");
+    }
+
+    public void SartDvdPlayer()
+    {
+        Console.WriteLine("Подготовка к просмотру DvdPlayer:");
+        dvdPlayer.PlaybackDvd();
+        audio.TurnOn();
+        audio.SetVolume(7);
+        Console.WriteLine("Dvd готов к просмотру");
+    }
+
+    public void EndTv()
+    {
+        Console.WriteLine("Фильм закончен");
+        tv.TurnOff();
+        audio.TurnOff();
+        Console.WriteLine("Выключение Tv");
+    }
+
+    public void EndDvd()
+    {
+        Console.WriteLine("Фильм закончен");
+        dvdPlayer.StopDvd();
+        audio.TurnOff();
+        Console.WriteLine("Выключение Dvd");
+    }
+
+    public void PlayGameStart() =>
+        gameConsole.StartGame();
+
+    public void PlayGameStop() =>
+        gameConsole.TurnOnGame();
+}
+
+#endregion
---
+++ HomeWork9/Project2_Composite/Program.cs
@@ -0,0 +1,83 @@
+﻿﻿var root = new MyDirectory("Root");
+
+var file1 = new MyFile("File1.txt", 1200);
+var file2 = new MyFile("File2.txt", 3400);
+
+var subDir = new MyDirectory("SubDirectory");
+var subFile1 = new MyFile("SubFile1.txt", 800);
+var subFile2 = new MyFile("SubFile2.txt", 1500);
+
+subDir.Add(subFile1);
+subDir.Add(subFile2);
+
+root.Add(file1);
+root.Add(file2);
+root.Add(subDir);
+
+Console.WriteLine("Структура файловой системы:\n");
+root.Display(1);
+
+Console.WriteLine($"\nОбщий размер папки Root: {root.GetSize()} байт");
+
+
+public abstract class FileSystemComponent(string name)
+{
+    public string Name { get; set; } = name;
+
+    public abstract void Display(int depth);
+    public abstract int GetSize();
+
+    public virtual void Add(FileSystemComponent component) => 
+        throw new NotImplementedException();
+
+    public virtual void Remove(FileSystemComponent component) => 
+        throw new NotImplementedException();
+}
+
+
+public class MyFile(string name, int size) : FileSystemComponent(name)
+{
+    public override void Display(int depth) => 
+        Console.WriteLine($"{new string('-', depth)} File: {Name}, Size: {size} байт");
+
+    public override int GetSize() => size;
+}
+
+
+public class MyDirectory(string name) : FileSystemComponent(name)
+{
+    private readonly List<FileSystemComponent> _children = new();
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine($"{new string('-', depth)} Directory: {Name}");
+        foreach (var component in _children)
+            component.Display(depth + 2);
+    }
+
+    public override int GetSize() => _children.Sum(c => c.GetSize());
+
+    public override void Add(FileSystemComponent component)
+    {
+        if (_children.Contains(component))
+        {
+            Console.WriteLine($"Компонент '{component.Name}' уже существует в '{Name}'!");
+            return;
+        }
+
+        _children.Add(component);
+        Console.WriteLine($"Добавлен компонент '{component.Name}' в папку '{Name}'");
+    }
+
+    public override void Remove(FileSystemComponent component)
+    {
+        if (!_children.Contains(component))
+        {
+            Console.WriteLine($"Компонент '{component.Name}' не найден в '{Name}'!");
+            return;
+        }
+
+        _children.Remove(component);
+        Console.WriteLine($"Удалён компонент '{component.Name}' из '{Name}'");
+    }
+}
